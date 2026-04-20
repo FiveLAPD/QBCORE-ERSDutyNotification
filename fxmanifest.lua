f8e4840f4cdf010eaba98c0d2a1b35c9fbfa8097
FiveLAPD <159000791+FiveLAPD@users.noreply.github.com>
2026-04-20 12:20:40 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'FiveLAPD'
+description 'ERS On/Off Duty Discord Webhook Bridge'
+version '1.0.0'
+
+server_scripts {
+    'config.lua',
+    'server.lua',
+}
